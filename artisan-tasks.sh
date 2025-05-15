679d258b12520b33a71e2f851ff7e8cff662c94a
iceeee <kenzo.lay1936@gmail.com>
2025-05-15 12:58:42 +0700

    fix:edit

--- artisan-tasks.sh
+++ artisan-tasks.sh
@@ -1,9 +1,1 @@
-php artisan key:generate
-php artisan storage:link
-php artisan migrate
-php artisan db:seed --class=AdminSeeder
-php artisan db:seed --class=ConfigSeeder
-php artisan config:clear
-php artisan route:clear
-php artisan view:clear
-php artisan cache:clear
+exec('php artisan key:generate');
